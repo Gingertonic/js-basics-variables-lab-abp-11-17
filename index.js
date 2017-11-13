8bb5c88ed436eb557304b08d57ced2267d5c3e75
Beth Schofield <bethmschofield@gmail.com>
2017-11-13 16:51:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const companyName = "Scuber";// Code your solution in this file!
 let mostProfitableNeighborhood = "Chelsea";
+<<<<<<< HEAD
 let companyCeo = "Susan Smith";
+=======
+let companyCeo = "Susan Smith";
+>>>>>>> b59ff1818e65baf5cd457b8b9e1559add1fe3348
